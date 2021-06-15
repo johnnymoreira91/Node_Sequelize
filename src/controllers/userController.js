f1ca3ae91a95473042e9c5d75f4113548a37c782
Johnny Moreira <jmoreira@engebras.com.br>
2021-06-15 20:38:10 -0300

    add password undefined

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,20 +2,24 @@ const User = require('../database/models/User');
 
 module.exports = {
 
-  async index(req,res) {
+  async index(req, res) {
     const user = await User.findAll()
     res.status(200).json(user)
   },
 
-  async store(req,res) {
-    const { name, email, password } = req.body
-
-    const user = await User.create({
-      name,
-      email,
-      password
-    })
-    res.status(200).json(user)
+  async store(req, res) {
+    try {
+      const { name, email, password } = req.body
+      const user = await User.create({
+        name,
+        email,
+        password
+      })
+      user.password = undefined
+      res.status(200).json(user)
+    } catch (error) {
+      res.status(401).json({message: 'Error to create a new User'})
+    }
   },
 
 }
