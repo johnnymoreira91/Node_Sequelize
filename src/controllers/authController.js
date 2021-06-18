f2d6368c59ff1726217b870918527fbd4d9c84f5
Johnny Moreira <jmoreira@engebras.com.br>
2021-06-17 21:20:43 -0300

    add page to login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../database/models/User');
 const jwt = require('jsonwebtoken');
 
 module.exports = {
-  documentation(req,res) {
+  documentation(req, res) {
     res.render('routes')
   },
   // login
@@ -11,7 +11,7 @@ module.exports = {
     try {
       const user = await User.findOne({ where: { email: email, password: password } })
       const accessToken = jwt.sign({ user: user.id }, process.env.SECRET, { expiresIn: 86400 })
-      
+
       console.log(accessToken)
       return res.json({
         message: `${user.name} has been authenticated`,
@@ -23,5 +23,5 @@ module.exports = {
       })
     }
   },
-// fim login
+  // fim login
 }
